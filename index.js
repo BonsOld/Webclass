d8c5c9b309d86dd09354a8989ce576cae75655d1
BonsOld <55369080+BonsOld@users.noreply.github.com>
2019-09-27 13:53:00 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+function myProjects() {
+    var x = document.getElementById("myProjects");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+function myPractices() {
+  var x = document.getElementById("myPractices");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function myBooks() {
+  var x = document.getElementById("myBooks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
